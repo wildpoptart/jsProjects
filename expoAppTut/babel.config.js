f36edbeaea569bcde0e0629b57cd914f347d4967
wildpoptart <porcshe33@gmail.com>
2023-01-25 21:01:51 -0500

    adding files

---
+++ expoAppTut/babel.config.js
@@ -0,0 +1,16 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
+};
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      "@babel/plugin-proposal-export-namespace-from",
+      "react-native-reanimated/plugin",
+    ],
+  };
+};
